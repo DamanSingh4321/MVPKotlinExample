10ed72b416449742edb3a4243f62e49b1c344dab
Daman <singhdaman4321@gmail.com>
2018-02-04 18:29:29 +0530

    feat: register dagger

--- app/src/main/java/com/singh/daman/mvpkotlinexample/InitApp.kt
+++ app/src/main/java/com/singh/daman/mvpkotlinexample/InitApp.kt
@@ -2,14 +2,24 @@ package com.singh.daman.mvpkotlinexample
 
 import android.app.Application
 import android.content.Context
+import com.singh.daman.mvpkotlinexample.di.component.ApplicationComponent
+import com.singh.daman.mvpkotlinexample.di.component.DaggerApplicationComponent
 
 /**
  * Created by Daman on 2/3/2018.
  */
 class InitApp : Application() {
 
+    private lateinit var applicationComponent: ApplicationComponent
+
     override fun onCreate() {
         super.onCreate()
+        setupComponent()
+    }
+
+    private fun setupComponent() {
+        applicationComponent = DaggerApplicationComponent.builder().build()
+        applicationComponent.inject(this)
     }
 
     fun getApplication(context: Context): InitApp {
